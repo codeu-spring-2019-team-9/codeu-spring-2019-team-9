cebc38b034d32c9c2e8c5f5fcee5d6e0118e814e
Mark <mser2@illinois.edu>
2019-02-23 21:36:25 -0600

    /stats page now returns total number of messages on the website:

--- src/main/java/com/google/codeu/servlets/StatsPageServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -5,15 +5,37 @@ import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.codeu.data.Datastore;
+import com.google.gson.JsonObject;
 /**
- * Responds with a hard-coded message for testing purposes.
+ * Handles fetching site statistics. 
+ * 
+ * This uses the init() function that creates and store a Datastore instance,
+ * then doGet() calls the function getTotalMessageCount() in the Datastore.java
+ * and create a JSON that countains the message count
  */
 public class StatsPageServlet extends HttpServlet{
- @Override
+  private Datastore datastore;
 
- public void doGet(HttpServletRequest request, HttpServletResponse response)
-   throws IOException {
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
 
-  response.getOutputStream().println("hello world");
- }
+  /**
+   * Responds with site statistics in JSON.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    response.setContentType("application/json");
+
+    int messageCount = datastore.getTotalMessageCount();
+
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.addProperty("messageCount", messageCount);
+    response.getOutputStream().println(jsonObject.toString());
+  }
 }
