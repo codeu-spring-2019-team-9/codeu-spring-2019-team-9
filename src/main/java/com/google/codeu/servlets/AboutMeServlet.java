870673e32f9e1c2269164a7bc11cf755432e37c9
Paulina González Dávalos <>
2019-02-25 11:05:07 -0600

    Fixed ERROR 404 on /about page, edited url-mapping

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -2,6 +2,9 @@ package com.google.codeu.servlets;
 
 import java.io.IOException;
 
+/*
+import javax.servlet.annotation.WebServlet;
+ */
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +17,6 @@ import com.google.codeu.data.Datastore;
 /**
  * Handles fetching and saving user data.
  */
-@WebServlet("/about")
 public class AboutMeServlet extends HttpServlet {
 
     private Datastore datastore;
