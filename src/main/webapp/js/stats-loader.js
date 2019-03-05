979b364202721dd9aa6187db64420449572a292a
Mark <mser2@illinois.edu>
2019-03-04 23:57:55 -0600

    followed up on suggestions on StatsPage

--- src/main/webapp/js/stats-loader.js
+++ src/main/webapp/js/stats-loader.js
@@ -1,23 +1,22 @@
-// Fetch stats and display them in the page.
-function fetchStats(){
-    const url = '/stats';
-    fetch(url).then((response) => {
+function fetchStats() {
+  const url = "/stats";
+  fetch(url)
+    .then(response => {
       return response.json();
-    }).then((stats) => {
-      const statsContainer = document.getElementById('stats-container');
-      statsContainer.innerHTML = '';
-      const messageCountElement = buildStatElement('Message count: ' + stats.messageCount);
+    })
+    .then(stats => {
+      const statsContainer = document.getElementById("stats-container");
+      statsContainer.innerHTML = "";
+      const messageCountElement = buildStatElement(
+        "Message count: " + stats.messageCount
+      );
       statsContainer.appendChild(messageCountElement);
     });
-  }
+}
 
-  function buildStatElement(statString){
-   const statElement = document.createElement('p');
-   statElement.appendChild(document.createTextNode(statString));
-   return statElement;
-  }
+function buildStatElement(statString) {
+  const statElement = document.createElement('p');
+  statElement.textContent = statString;
+  return statElement;
+}
 
-  // Fetch data and populate the UI of the page.
-  function buildUI(){
-   fetchStats();
-  }
